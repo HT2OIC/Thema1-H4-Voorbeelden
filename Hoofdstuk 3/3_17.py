662709630c9704630d85edffeeefab05772d5bc2
VERDOSTM <dorien.vervoort@stm.kobart.be>
2025-09-18 11:32:03 +0200

    Aanpassingen

--- Hoofdstuk 3/3_17.py
+++ Hoofdstuk 3/3_17.py
@@ -1,7 +1,7 @@
 antwoord = input("Wie schilderde de Mona Lisa?")
 
 # Controleren op kernwoord
-if "leonardo" in antwoord.lower():
+if "Leonardo da Vinci".lower() in antwoord.lower():
     print("Proficiat! Je hebt de vraag juist beantwoord!")
 else:
     print("Jammer! Het juiste antwoord was: Leonardo da Vinci")
