9df534cb3fdd0745ffa7cc8d3e969230eb90d287
VERDOSTM <dorien.vervoort@stm.kobart.be>
2026-06-29 14:05:13 +0200

    Aanpassing H3

--- Hoofdstuk 3/3_1.py
+++ Hoofdstuk 3/3_1.py
@@ -13,4 +13,4 @@ antwoord_aardrijkskunde = "Mars"
 antwoord_kunst_literatuur = "Leonardo da Vinci"
 
 # Willekeurig vraagnummer kiezen (1 tem 5)
-nummer_vraag = random.randint(1, 5)
+vraag = random.randint(1, 5)
